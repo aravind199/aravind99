b71255a9ecd0ffe6a08c9a35f91b5f877c0a5a0b
aravind199 <52271337+aravind199@users.noreply.github.com>
2019-08-05 14:06:38 +0530

    Create pro1.py

---
+++ pro1.py
@@ -0,0 +1,12 @@
+g1=int(input())
+df=[]
+for i in range(0,g1):
+ ij=input()
+ df.append(ij)
+ff=[]
+for i in zip(*df):
+ if(i.count(i[0])==len(i)):
+  ff.append(i[0])
+ else:
+  break
+print(''.join(ff))
